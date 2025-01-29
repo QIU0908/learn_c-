99088f684803463bb2f7284a65d20230088545ea
QIU0908 <a0985012033@gmail.com>
2025-01-29 21:49:55 +0800

    add chat 2

---
+++ chat2/ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+﻿//tast two
+#include <iostream>
+using namespace std;
+int main() {
+	int a , b;
+	a = 8;
+	b = 10;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	cout << a<<"-"<<b<<"="<<a-b<<endl;
+	return 0;
+}
